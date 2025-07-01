ddf90e1d04e29768b1d9eb237083a9e409eab7db
Gururagavendra85 <gururagavendra.v@rapyuta-robotics.com>
2025-07-01 22:33:15 +0530

    pointers assignment 1

--- pointers/tasks/dynamic-array/solution.h
+++ pointers/tasks/dynamic-array/solution.h
@@ -1,3 +1,49 @@
 #pragma once
+#include <iostream>
 
 // Write your solution here
+int* createArray(size_t size, int initialValue){
+    if(size<1){
+        int* arr = nullptr;
+        return arr; 
+    }
+    int* arr = new int[size];
+    for(int i=0;i<size;i++){
+        arr[i]=initialValue;
+    }
+    return arr;
+}
+
+int* copyArray(const int* source, size_t size){
+    if(size<1){
+        int* arr = nullptr;
+        return arr; 
+    }
+    int* arr = new int[size];
+    for (size_t i = 0; i < size; ++i) {
+        arr[i] = source[i];
+    }
+    return arr;
+}
+
+
+//[1,2,3,4,5]
+//[5,4,3,2,1]
+void reverseArray(int* array, size_t size){
+    int i=0;
+    int n = size -1;
+    int temp = 0;
+    while (i!=n)
+    {
+        temp = array[n];
+        array[n] = array[i];
+        array[i] = temp;
+        n = n-1;
+        i = i+1;
+    } 
+    
+}
+
+void deleteArray(int* array){
+    delete[] array;
+}
