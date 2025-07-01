c55970f55089f256a8512fbdf39ae01cb11b9d73
Gururagavendra85 <gururagavendra.v@rapyuta-robotics.com>
2025-07-01 19:41:01 +0530

    assignment 7

--- basics/tasks/polygon/include/solution.h
+++ basics/tasks/polygon/include/solution.h
@@ -1,3 +1,36 @@
 #pragma once
 
 // Write your solution here
+class Polygon{
+protected:
+    int width;
+    int height;
+public:
+    virtual int getArea()=0; //Abstract base class
+};
+
+class Rectangle: public Polygon{
+    public:
+    Rectangle(int width,int height){
+        this->width = width;
+        this->height = height;
+        
+    }
+
+    int getArea() override{
+        return width*height;
+    }
+};
+
+
+class RightTriangle:public Polygon{
+    public:
+    RightTriangle(int width,int height){
+        this->width = width;
+        this->height = height;  
+    }
+
+    int getArea() override{
+        return 0.5*(width*height);
+    }
+};
