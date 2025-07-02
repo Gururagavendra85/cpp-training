f4763ecbc5935e3b0ad1f81dc6d6f814bf3bf4ec
Gururagavendra85 <gururagavendra.v@rapyuta-robotics.com>
2025-07-02 08:51:42 +0530

    simple vector assignment

--- pointers/tasks/simple-vector/main.cpp
+++ pointers/tasks/simple-vector/main.cpp
@@ -1,7 +1,21 @@
 #include "solution.h"
+#include <iostream>
+#include <stdexcept>
 
 int main() {
-    // Write a program that uses the IntVector class
-    // It can be as simple as asking user to input numbers and then printing them back
+    
+    // Create a new IntVector
+    IntVector vec;
+    std::cout << "Created a new IntVector. Current size: " << vec.size() << "\n";
+    
+    // Add some elements
+    std::cout << "Adding elements: 10, 20, 30\n";
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    
+    std::cout << "Current size: " << vec.size() << "\n";
+    
+    return 0;
 }
 
--- pointers/tasks/simple-vector/solution.h
+++ pointers/tasks/simple-vector/solution.h
@@ -1,3 +1,55 @@
 #pragma once
+#include <iostream>
+#include <stdexcept>
 
-// Your solution here
+// Simple vector implementation for integers
+class IntVector {
+private:
+    int* data;       // Pointer to the dynamic array
+    size_t capacity; // The allocated size of the array
+    size_t count;    // The number of elements currently in the array
+
+
+    void resize(size_t new_capacity){
+        int* newarr= new int[new_capacity]{};
+
+        for(int i=0;i<count;i++){
+                newarr[i]=data[i];
+        }
+
+        delete[] data;
+        data = newarr;
+        capacity = new_capacity;
+    
+    }
+public:
+    // Default constructor
+    IntVector() : data(new int[0]), capacity(0), count(0) {}
+    
+    IntVector(size_t size) : data(new int[size]), capacity(size), count(0) {}
+
+    void push_back(int value) {
+        if (count >= capacity) {
+            size_t new_capacity = capacity == 0 ? 1 : capacity * 2;
+            resize(new_capacity);
+        }
+        
+        data[count] = value;
+        ++count;
+    }
+
+    int at(size_t index) {
+        if (index >= count) {
+            throw std::out_of_range("Index out of range");
+        }
+        return data[index];
+    }
+
+    size_t size() const {
+        return count;
+    }
+
+    ~IntVector() {
+        delete[] data;
+    }
+};
