eaa51585bdc8657ea74d3051e927cefef66c8c2d
Gururagavendra85 <gururagavendra.v@rapyuta-robotics.com>
2025-07-10 21:01:17 +0530

    implement unique pointer

--- pointers/tasks/simple-unique-ptr/main.cpp
+++ pointers/tasks/simple-unique-ptr/main.cpp
@@ -1,11 +1,7 @@
 #include "solution.h"
 #include <iostream>
 
-struct Test {
-    int value;
-    Test(int v) : value(v) {}
-    void show() { std::cout << "Value: " << value << std::endl; }
-};
+
 
 int main() {
     SimpleUniquePtr<Test> ptr(new Test(10));
--- pointers/tasks/simple-unique-ptr/solution.h
+++ pointers/tasks/simple-unique-ptr/solution.h
@@ -1,3 +1,101 @@
 #pragma once
-
+#include<iostream>
 // Your solution here
+struct Test {
+    int value;
+    Test(int v) : value(v) {}
+    void show() { std::cout << "Value: " << value << std::endl; }
+};
+
+template <typename T>
+class SimpleUniquePtr {
+    private:
+    T* _ptr = nullptr;
+
+    public:
+    SimpleUniquePtr(T* ptr = nullptr):_ptr(ptr){};
+
+    // copy constructor and copy assignment should be disabled
+    SimpleUniquePtr(SimpleUniquePtr& t) = delete;
+    SimpleUniquePtr& operator=(SimpleUniquePtr& t) = delete;
+
+    // move cons. and move ass is possible
+    SimpleUniquePtr(SimpleUniquePtr&& t){
+        _ptr = t._ptr;
+        t._ptr = nullptr;
+    }
+
+    SimpleUniquePtr& operator=(SimpleUniquePtr&& t){
+        if(this!=&t){
+            delete _ptr;
+            _ptr = t._ptr;
+            t._ptr = nullptr;
+        }
+        return *this;
+    }
+
+    // operator overloading for derefernce operators for pointers * and -> 
+    T* operator->(){
+        return _ptr;
+    }
+
+    T& operator*(){
+        return *_ptr;
+    }
+
+
+    T* get(){
+        return _ptr;
+    }
+
+    //destructor
+    ~SimpleUniquePtr(){
+        delete _ptr;
+    }
+
+};
+
+
+template<>
+class SimpleUniquePtr<Test>{
+    private:
+    Test* _ptr = nullptr;
+
+    public:
+    SimpleUniquePtr(Test* ptr = nullptr):_ptr(ptr){};
+
+    // copy constructor and copy assignment should be disabled
+    SimpleUniquePtr(SimpleUniquePtr& t) = delete;
+    SimpleUniquePtr& operator=(SimpleUniquePtr& t) = delete;
+
+    // move cons. and move ass is possible
+    SimpleUniquePtr(SimpleUniquePtr&& t){
+        _ptr = t._ptr;
+        t._ptr = nullptr;
+    }
+
+    SimpleUniquePtr& operator=(SimpleUniquePtr&& t){
+        if(this!=&t){
+            delete _ptr;
+            _ptr = t._ptr;
+            t._ptr = nullptr;
+        }
+
+        return *this;
+    }
+
+    // operator overloading for derefernce operators for pointers * and -> 
+    Test* operator->(){
+        return _ptr;
+    }
+
+    Test& operator*(){
+        return *_ptr;
+    }
+
+    //destructor
+    ~SimpleUniquePtr(){
+        delete _ptr;
+    }
+
+};
