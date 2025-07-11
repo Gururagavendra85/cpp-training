4ddabb07fa3252d61ddb8e296b5cc9c855fb8d9e
Gururagavendra85 <gururagavendra.v@rapyuta-robotics.com>
2025-07-11 10:46:08 +0530

    polymorphism asignment

--- pointers/tasks/zoo/solution.h
+++ pointers/tasks/zoo/solution.h
@@ -1,4 +1,23 @@
 #pragma once
 #include "animal.h"
+#include <vector>
+#include <memory>
+
 
 // Write Zoo class here
+class Zoo{
+    private:
+        std::vector<std::unique_ptr<Animal>> vec;
+
+    public:
+    void addAnimal(Animal* obj){
+        vec.push_back(std::unique_ptr<Animal>(obj));
+    }
+
+    void showAllAnimals(){
+        for(const std::unique_ptr<Animal>& x: vec){
+            x->printInfo();
+            x->makeSound();
+        }
+    }
+};
